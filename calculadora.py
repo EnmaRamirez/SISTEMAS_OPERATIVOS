af2c839473dd4b1ac806e957b123faee5a10fe23
Leticia <eramirezc38@miumg.edu.gt>
2025-08-06 23:20:51 -0600

    Versión 5: Agregar menú de bienvenida

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,1 @@
-def sumar(a, b): return a + bndef restar(a, b): return a - bndef multiplicar(a, b): return a * bndef dividir(a, b): return a / b if b != 0 else 'Error'
+def sumar(a, b): return a + bndef restar(a, b): return a - bndef multiplicar(a, b): return a * bndef dividir(a, b): return a / b if b != 0 else "Error"nnprint("Bienvenido a la calculadora")
